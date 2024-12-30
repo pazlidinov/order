1e2ac5c127a795420079aa31bbf2017b78ab91d8
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2024-12-30 10:35:42 +0500

    A function to download the bot from social networks has been prepared.

---
+++ download_bot/handlers/users/main.py
@@ -0,0 +1,69 @@
+import os
+from aiogram import types
+from aiogram.utils import executor
+from loader import dp
+import yt_dlp
+from moviepy.editor import VideoFileClip, AudioFileClip
+
+
+async def download_from_youtube(url):
+    # Video yuklash
+    ydl_opts_video = {
+        "format": "bestvideo",
+        "outtmpl": "downloads/video.mp4",
+    }
+    with yt_dlp.YoutubeDL(ydl_opts_video) as ydl:
+        ydl.download([url])
+
+    # Audio yuklash
+    ydl_opts_audio = {
+        "format": "bestaudio",
+        "outtmpl": "downloads/audio.m4a",
+    }
+    with yt_dlp.YoutubeDL(ydl_opts_audio) as ydl:
+        ydl.download([url])
+
+    # Video va audio fayllarni yuklash
+    video_clip = VideoFileClip("downloads/video.mp4")
+    audio_clip = AudioFileClip("downloads/audio.m4a")
+
+    # Videoga audiolarni qo'shish
+    final_clip = video_clip.set_audio(audio_clip)
+
+    # Yangi video faylni saqlash
+
+    final_clip.write_videofile(
+        "downloads/output.mp4", codec="libx264", audio_codec="aac"
+    )
+    os.remove("downloads/video.mp4")
+    os.remove("downloads/audio.m4a")
+    return "downloads/output.mp4"
+
+
+async def download_from_others(url):
+    yt_opts = {"outtmpl": "downloads/%(title)s.%(ext)s"}
+    with yt_dlp.YoutubeDL(yt_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=True)
+        return ydl.prepare_filename(info_dict)
+
+
+@dp.message_handler(regexp=r"https?://[^\s]+")
+async def download_video(message: types.Message):
+    video_url = message.text
+    await message.reply("Videoni yuklab olishni boshlayapman. Bir oz kuting...")
+
+    # Videoni yuklab olish
+    if "youtu" in video_url:
+        video_path = await download_from_youtube(video_url)
+    else:
+        video_path = await download_from_others(video_url)
+
+    if video_path:
+        # Foydalanuvchiga videoni yuborish
+        with open(video_path, "rb") as video:
+            await message.reply_video(video, caption="Mana sizning videongiz!")
+            os.remove(video_path)
+    else:
+        await message.reply(
+            "Videoni yuklab bo'lmadi. Iltimos, linkni tekshiring yoki boshqa video yuboring."
+        )
