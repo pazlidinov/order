0f4cfafa51e6ae4014d7bdf36846b955d495ecec
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2025-01-05 08:52:02 +0500

    inline btn created

--- download_bot/handlers/users/main.py
+++ download_bot/handlers/users/main.py
@@ -3,12 +3,14 @@ from aiogram import types
 from loader import dp, bot
 import yt_dlp
 from moviepy.editor import VideoFileClip, AudioFileClip
+import logging
+from keyboards.inline.forward import forward_to_others
 
 
 async def download_from_youtube(url):
     # Video yuklash
     ydl_opts_video = {
-        "format": "bestvideo",
+        "format": "160",
         "outtmpl": "downloads/video.mp4",
     }
     with yt_dlp.YoutubeDL(ydl_opts_video) as ydl:
@@ -46,18 +48,8 @@ async def download_from_others(url):
         return ydl.prepare_filename(info_dict)
 
 
-@dp.message_handler(regexp=r"https?://[^\s]+")
-async def download_video(message: types.Message):
-    video_url = message.text
+async def send_video(message, video_path):
     bot_username = (await bot.get_me()).username
-    await message.reply("Videoni yuklab olishni boshlayapman. Bir oz kuting...")
-
-    # Videoni yuklab olish
-    video_path = (
-        await download_from_youtube(video_url)
-        if "youtu" in video_url
-        else await download_from_others(video_url)
-    )
 
     if video_path:
         # Foydalanuvchiga videoni yuborish
@@ -65,9 +57,40 @@ async def download_video(message: types.Message):
             await message.reply_video(
                 video,
                 caption=f"\nBot manzili - <a href='https://t.me/{bot_username}?start={message.from_user.id}'>{bot_username}</a>\n",
+                reply_markup=forward_to_others,
             )
             os.remove(video_path)
     else:
         await message.reply(
             "Videoni yuklab bo'lmadi. Iltimos, linkni tekshiring yoki boshqa video yuboring."
         )
+
+
+@dp.message_handler(regexp=r"https?://[^\s]+")
+async def accept_link(message: types.Message):
+    video_url = message.text
+    if "youtu" in video_url:
+        try:
+            # yt-dlp yordamida formatlarni olish
+            ydl_opts = {"quiet": True}  # Konsol chiqishini o'chirish
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(video_url, download=False)
+                formats = info.get("formats", [])
+
+                list_formats = {}
+                for fmt in formats:
+                    if "p)" in fmt.get("format"):
+                        one_format = fmt.get("format").split(" ")
+                        list_formats[one_format[-1]] = one_format[0]
+                logging.info(list_formats)
+                # if not formats:
+                #     response = "Formatlar topilmadi."
+        except Exception as e:
+            await message.reply(f"Xato yuz berdi")
+        # await message.reply("Videoni yuklab olishni boshlayapman. Bir oz kuting...")
+        # video_path = await download_from_youtube(video_url)
+        # await send_video(message, video_path)
+    else:
+        await message.reply("Videoni yuklab olishni boshlayapman. Bir oz kuting...")
+        video_path = await download_from_others(video_url)
+        await send_video(message, video_path)
---
+++ download_bot/keyboards/inline/forward.py
@@ -0,0 +1,7 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+forward_to_others = InlineKeyboardMarkup(
+    inline_keyboard=[
+        InlineKeyboardButton(text="Ulashish", switch_inline_query="Zo'r bot ekan"),
+    ]
+)
